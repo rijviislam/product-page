9a211e2aa5c83e900e6062e4a9e5af3e5da54931
rijviislam <rijviislam54@gmail.com>
2025-05-09 15:41:59 +0600

    feat: added drawer

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const incrementBtn = document.getElementById("incrementBtn");
 const decrementBtn = document.getElementById("decrementBtn");
 const quantity = document.querySelector(".quantity-value");
 const price = document.getElementById("price");
+const productTitle = document.getElementById("title");
+const productImg = document.getElementById("mainImg");
+const imgSrc = productImg.src;
+const title = productTitle.innerText;
+const productPrice = parseInt(price.innerText);
+const cartContainer = document.getElementById("cartContainer");
+
 cartBtn.addEventListener("click", () => {
   drawer.classList.remove("display-none");
 });
@@ -15,17 +22,17 @@ closeDrawer.addEventListener("click", () => {
 });
 
 incrementBtn.addEventListener("click", () => {
-    const productPrice = 249.00;
+  const productPrice = 249.0;
   if (quantity.textContent == 10) {
     alert("Quantity cannot be greater than 10");
     return;
   }
-  quantity.textContent = parseInt(quantity.textContent) + 1;
-  price.textContent = parseInt(price.textContent) + parseInt(productPrice);
+  quantity.textContent = parseFloat(quantity.textContent) + 1;
+  price.textContent = parseFloat(price.textContent) + parseFloat(productPrice);
 });
 
 decrementBtn.addEventListener("click", () => {
-    const productPrice = 249.00;
+  const productPrice = 249.0;
   if (quantity.textContent == 1) {
     alert("Quantity must be at least 1");
     return;
@@ -33,3 +40,30 @@ decrementBtn.addEventListener("click", () => {
   quantity.textContent = parseInt(quantity.textContent) - 1;
   price.textContent = parseInt(price.textContent) - parseInt(productPrice);
 });
+
+const handleCart = function () {
+  const cartItem = {
+    title: title,
+    price: productPrice,
+    img: imgSrc,
+  };
+  localStorage.setItem("cart", JSON.stringify(cartItem));
+
+  const getCart = localStorage.getItem("cart");
+  if (getCart) {
+    const productObj = JSON.parse(getCart);
+    const title = productObj.title;
+    const price = productObj.price;
+    const imgSrc = productObj.img;
+    cartContainer.innerHTML = `<div class="drawer-item">
+    <img src="${imgSrc}" alt="Product Image"/>
+    <div class="price-title">
+    <h4>${title}</h4>
+    <p>$ ${price}</p>
+    </div>
+    <p>$ ${price}</p>
+    </div>
+    
+    `;
+  }
+};
